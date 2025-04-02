d6dd9f59220e8a75add3e03de0cc4a1245fa10b1
Nihal-997 <nihalchapekar@gmail.com>
2025-04-02 20:15:04 +0530

    use of where query in sql

---
+++ use of where.sql
@@ -0,0 +1,103 @@
+select * from restaurants
+
+select name,rating,contact from restaurants
+
+select * from staff
+
+
+select DISTINCT stafftype from 
+
+select DISTINCT  bloodgrp,stafftype from staff
+
+select * from staff where stafftype = 'cheff'
+
+select * from staff where bloodgrp = 'O+'
+
+select name from staff where bloodgrp = 'O+'
+
+
+select * from orders
+
+select DISTINCT restaurantid from orders where cusine = 'Indian'
+
+
+     
+	 
+	 
+	 
+	  select * from staff
+
+
+select * from staff where salary = 62000
+
+select * from staff where bloodgrp = 'AB+'
+
+select * from staff where stafftype = 'maneger'
+
+select * from staff where restaurantid = 3
+
+select * from staff where name = 'captain'
+
+select * from staff where waiter bloodgrp = 'A+'
+
+select * from staff where salary = 47000
+
+select * from staff where id = 10
+
+        select * from customer
+
+select * from customer where name = 'Emily Davis'
+
+select * from customer where restaurantid = 2
+
+select * from customer where id = 4
+
+select * from customer where id = 7
+
+select * from customer where contact = 9123456780
+
+select * from customer where name = 'Jane Smith'
+
+select * from customer where email = 'john.doe@example.com'
+
+select * from customer where name = 'Robert Brown'
+
+      select * from orders
+
+select * from orders where qty = 4
+
+select * from orders where tableno = 13
+
+select * from orders where cust_id = 6
+
+select * from orders where id = 12
+
+select * from orders where cusine =' Dessert'
+
+select * from orders where dish = 'BBQ Ribs'
+
+select * from orders where dish = 'Pasta'
+
+select * from orders where waiter = 7
+
+
+      select * from restaurants
+
+select * from restaurants where rating = 5
+
+select * from restaurants where name = 'Taco Tales'
+
+select * from restaurants where cuisine = '{American}'
+
+select * from restaurants where rating = 3
+
+select * from restaurants where name = 'Grill Master'
+
+select * from restaurants where optime = '10:00:00'
+
+select * from restaurants where closingtime = '21:00:00'
+
+select * from restaurants where cuisine = '{Barbecue}'
+
+
+
